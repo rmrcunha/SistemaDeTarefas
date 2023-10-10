1d63c60b9cbd221873c02dc3e31c97b3f8381469
Renan Cunha <renancdev@gmail.com>
2023-10-10 17:13:51 -0300

    CRUD usuario e tarefas prontos

--- Tarefas/Repositorios/Interfaces/IUsuarioRepositorio.cs
+++ Tarefas/Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -4,7 +4,7 @@ namespace Tarefas.Repositorios.Interfaces
 {
     public interface IUsuarioRepositorio
     {
-        Task<List<UsuarioModel>> BuuscarTodosUsuarios();
+        Task<List<UsuarioModel>> BuscarTodosUsuarios();
         Task<UsuarioModel> BuscarPorId(int id);
         Task<UsuarioModel> Adicionar(UsuarioModel usuario);
         Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id);
